d4f0f3aa8cdf0794aec36ea72158517e8490252f
Mohatoto <mohatoto226@gmail.com>
2025-04-11 12:05:57 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,8 @@
-// 📦 RootLayout required for Next.js App Router
+export const metadata = {
+  title: "VIP Recharge Shop",
+  description: "Recharge your KSA apps like Kiyo, TikTok, Mr7ba & more",
+};
+
 export default function RootLayout({
   children,
 }: {
